ffa765f91533899d1deae0bd26edef829924aa16
mbarnaba <matteo.barnaba1@gmail.com>
2022-03-03 20:44:36 +0100

    added support for GTest and implemented test on ekin()

---
+++ tests/test_kinetic.cpp
@@ -0,0 +1,41 @@
+// unit test example with test fixture
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "force.h"
+}
+
+
+class TestKinetic: public ::testing::Test {
+    protected:
+    mdsys_t* sys;
+
+    void SetUp() {
+        sys = new mdsys_t;
+        sys->natoms = 2;
+        sys->mass = 1.0;
+        sys->rx = new double[2];
+        sys->vx = new double[2];
+        sys->fx = new double[2];
+        sys->rx[0] = -1.0;
+        sys->rx[1] = 1.0;
+        sys->vx[0] = 1.0;
+        sys->vx[1] = 0.0;
+        sys->fx[0] = 1.0;
+        sys->fx[1] = 0.2;
+    }
+
+    void TearDown() {
+        delete[] sys->rx;
+        delete[] sys->vx;
+        delete[] sys->fx;
+
+        delete sys;
+    }
+};
+
+TEST_F(TestKinetic, kinetic) {
+    const double expected{ 0.5 * sys->mass * sys->vx[ 0 ] * sys->vx[ 0 ] * mvsq2e }; 
+    ekin( sys );  
+    ASSERT_DOUBLE_EQ( sys->ekin, expected ); 
+}

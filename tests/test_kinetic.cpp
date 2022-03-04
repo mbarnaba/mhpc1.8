64cb65faf5c187e7eaad04eb59741d788126d3d9
Marco Celoria <celoria.marco@gmail.com>
2022-03-04 13:29:42 +0100

    kinetic test fixed? + force micro optimization

--- tests/test_kinetic.cpp
+++ tests/test_kinetic.cpp
@@ -16,11 +16,17 @@ class TestKinetic: public ::testing::Test {
         sys->mass = 1.0;
         sys->rx = new double[2];
         sys->vx = new double[2];
+	sys->vy = new double[2];
+	sys->vz = new double[2];
         sys->fx = new double[2];
         sys->rx[0] = -1.0;
         sys->rx[1] = 1.0;
         sys->vx[0] = 1.0;
         sys->vx[1] = 0.0;
+	sys->vy[0] = 0.0;
+        sys->vy[1] = 0.0;
+	sys->vz[0] = 0.0;
+        sys->vz[1] = 0.0;
         sys->fx[0] = 1.0;
         sys->fx[1] = 0.2;
     }
@@ -35,7 +41,7 @@ class TestKinetic: public ::testing::Test {
 };
 
 TEST_F(TestKinetic, kinetic) {
-    const double expected{ 0.5 * sys->mass * sys->vx[ 0 ] * sys->vx[ 0 ] * mvsq2e }; 
+    const double expected{ 0.5 * sys->mass * sys->vx[ 0 ] * sys->vx[ 0 ] * mvsq2e };
     ekin( sys );  
     ASSERT_DOUBLE_EQ( sys->ekin, expected ); 
 }

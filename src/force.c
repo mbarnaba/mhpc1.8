e65339088eb9fef9e4d42ae29a94853362aa9cef
mhasan49 <mhaasan49physics@gmail.com>
2022-03-05 11:47:25 +0100

    omp fix

--- src/force.c
+++ src/force.c
@@ -21,14 +21,26 @@ double pbc(double x, const double boxby2,const double twice_boxby2)
 
 
 /* compute kinetic energy */
+
 void ekin(mdsys_t *sys)
 {   
     int i;
 
+
+    double ekin = 0.0;
+    const double prefactor = 0.5 * mvsq2e * sys->mass;
+
     sys->ekin=0.0;
+    
+    #ifdef _OPENMP
+    #pragma omp parallel for default(shared) private(i) reduction(+ : ekin)
+    #endif
     for (i=0; i<sys->natoms; ++i) {
-        sys->ekin += 0.5*mvsq2e*sys->mass*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
+
+        ekin += prefactor*(sys->vx[i]*sys->vx[i] + sys->vy[i]*sys->vy[i] + sys->vz[i]*sys->vz[i]);
+
     }
+    sys->ekin = ekin;
     sys->temp = 2.0*sys->ekin/(3.0*sys->natoms-3.0)/kboltz;
 }
 
@@ -44,6 +56,7 @@ void force(mdsys_t *sys)
     const double half_box = 0.5*sys->box;
     const double twice_boxby2 = sys->box;
     /* zero energy and forces */
+    double local_epot = 0.0;
     sys->epot=0.0;
     azzero(sys->fx,sys->natoms);
     azzero(sys->fy,sys->natoms);
@@ -56,9 +69,6 @@ void force(mdsys_t *sys)
     const double  rcsq = sys->rcut * sys->rcut;
     double rsq, rinv, r6;
     double rxffac, ryffac, rzffac;
-#ifdef _OPENMP
-#pragma omp parallel for 
-#endif //_OPENMP
     for(i=0; i < (sys->natoms - 1); ++i) {
         for(j=i+1; j < (sys->natoms); ++j) {
             /* particles have no interactions with themselves */
@@ -72,7 +82,7 @@ void force(mdsys_t *sys)
                 rinv=1.0/rsq;
                 r6 = rinv*rinv*rinv;
                 ffac = (12.0*c12*r6 - 6.0*c6)*r6*rinv;
-                sys->epot  += r6*(c12*r6 - c6);
+                local_epot  += r6*(c12*r6 - c6);
                 rxffac = rx*ffac;
                 sys->fx[i] += rxffac;
                 sys->fx[j] -= rxffac;
@@ -85,6 +95,7 @@ void force(mdsys_t *sys)
             }
         }
     }
+    sys->epot = local_epot;
 }
 #elif defined(_NEWTON)
 /* force using Newton's third law */
@@ -100,10 +111,6 @@ void force(mdsys_t *sys)
     azzero(sys->fx,sys->natoms);
     azzero(sys->fy,sys->natoms);
     azzero(sys->fz,sys->natoms);
-    //Constants added to avoid computing pow,sqrt etc.
-   #ifdef _OPENMP
-   #pragma omp parallel for 
-   #endif //_OPENMP
     for(i=0; i < (sys->natoms - 1); ++i) {
         for(j=i+1; j < (sys->natoms); ++j) {
             /* particles have no interactions with themselves */
@@ -137,12 +144,13 @@ void force(mdsys_t *sys)
     const double half_box = 0.5*sys->box;
     const double twice_boxby2 = sys->box;
     /* zero energy and forces */
+    double tmp_epot = 0.0;
     sys->epot=0.0;
     azzero(sys->fx,sys->natoms);
     azzero(sys->fy,sys->natoms);
     azzero(sys->fz,sys->natoms);
     #ifdef _OPENMP
-    #pragma omp parallel for 
+    #pragma omp parallel for default(shared) schedule(dynamic) private(i, j, rx, ry, rz, r, ffac) reduction(+:tmp_epot)
     #endif //_OPENMP
     for(i=0; i < (sys->natoms); ++i) {
         for(j=0; j < (sys->natoms); ++j) {
@@ -154,15 +162,18 @@ void force(mdsys_t *sys)
             rz=pbc(sys->rz[i] - sys->rz[j], half_box,twice_boxby2);
             r = sqrt(rx*rx + ry*ry + rz*rz);
             /* compute force and energy if within cutoff */
+           
             if (r < sys->rcut) {
                 ffac = -4.0*sys->epsilon*(-12.0*pow(sys->sigma/r,12.0)/r+6*pow(sys->sigma/r,6.0)/r);
-                sys->epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)-pow(sys->sigma/r,6.0));
+                tmp_epot += 0.5*4.0*sys->epsilon*(pow(sys->sigma/r,12.0)-pow(sys->sigma/r,6.0));
+                
                 sys->fx[i] += rx/r*ffac;
                 sys->fy[i] += ry/r*ffac;
                 sys->fz[i] += rz/r*ffac;
             }
         }
     }
+     sys->epot = tmp_epot;
 }
 #endif
 #else // _MPI 
